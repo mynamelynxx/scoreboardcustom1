1807c0e5767104d29ff1784bf0d549f1543a7f99
mynamelynxx <mydr040506@gmail.com>
2026-04-06 13:11:36 +0400

    Create PlayerListHudMixin.java

---
+++ src/client/java/com/scoreboardchanger/mixin/PlayerListHudMixin.java
@@ -0,0 +1,8 @@
+package com.scoreboardchanger.mixin;
+
+import net.minecraft.client.gui.hud.PlayerListHud;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerListHud.class)
+public class PlayerListHudMixin {
+}
