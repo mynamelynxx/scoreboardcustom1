c6ae5c04d2e0436736b15e099005dc93afad16cd
mynamelynxx <mydr040506@gmail.com>
2026-04-06 13:10:59 +0400

    Delete src/main/java/com/scoreboardchanger/mixin/PlayerListHudMixin.java

--- src/main/java/com/scoreboardchanger/mixin/PlayerListHudMixin.java
+++
@@ -1,9 +0,0 @@
-package com.scoreboardchanger.mixin;
-
-import net.minecraft.client.gui.hud.PlayerListHud;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PlayerListHud.class)
-public class PlayerListHudMixin {
-    // Reserved for future use
-}
